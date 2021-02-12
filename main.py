edf89a62f5d9fe195e3521b91ddd92170c0665e1
MetabaronGit <76850166+MetabaronGit@users.noreply.github.com>
2021-02-12 19:43:34 +0100

    přidán login

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from task_template import TEXTS
 
-print(TEXTS[2])
+login_tab = dict(bob = "123", ann = "pass123", mike = "password123", liz = "pass123")
+login_name = str(input("Zadej svoje přihlašovací jméno: "))
+login_password = str(input("Heslo: "))
+
+if login_tab.get(login_name) == login_password:
+    print("Dobrý den {0}, analyzátor textu je připraven.\n".format(login_name))
+
+
+else:
+    print("Nesprávné jméno nebo heslo. Přístup zamítnut!")
+
+ # print(login_tab[login_name])
