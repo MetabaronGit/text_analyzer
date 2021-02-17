f40b5c6240eb455ec5ce5d6693283d0425a55b75
MetabaronGit <76850166+MetabaronGit@users.noreply.github.com>
2021-02-17 22:14:01 +0100

    zapracování připomínek

--- main.py
+++ main.py
@@ -14,42 +14,30 @@ if login_tab.get(login_name) == login_password:
     print("Vítej v aplikaci, {0}.".format(login_name))
     print("Počet připravených textů k analýze: {0}".format(len(TEXTS)))
     print(LINE_SEPARATOR)
-    text_id = input("Který text chceš analyzovat? Zadej číslo 1 - 3: ")
+    text_id = int(input("Který text chceš analyzovat? Zadej číslo 1 - 3: "))
     print(LINE_SEPARATOR)
 
-    if text_id.strip() in ("1", "2", "3"):
+    if text_id in range(1, 4):
         #hlavní analýza
+        word_list = TEXTS[text_id - 1].split()
+        for i in range(len(word_list)):
+            word_list[i] = word_list[i].strip(" \".,")
+
         len_occurences = dict()
-        analyzed_words = dict(total_words=0,
-                              titlecase_words=0,
-                              uppercase_words=0,
-                              lowercase_words=0,
-                              numbers=0,
-                              numbers_sum=0)
-        actual_word = ""
+        analyzed_words = dict()
 
-        for char in TEXTS[int(text_id) - 1]:
-            if char.isalnum():
-                actual_word += char
-            else:
-                if actual_word != "":
-                    analyzed_words["total_words"] += 1
-                    if actual_word.isalpha():
-                        if actual_word[0].isupper():
-                            analyzed_words["titlecase_words"] += 1
-                        if actual_word.isupper():
-                            analyzed_words["uppercase_words"] += 1
-                        if actual_word.islower():
-                            analyzed_words["lowercase_words"] += 1
-                    if actual_word.isnumeric():
-                        analyzed_words["numbers"] += 1
-                        analyzed_words["numbers_sum"] += int(actual_word)
-
-                    if len_occurences.get(str(len(actual_word))) is not None:
-                        len_occurences[str(len(actual_word))] += 1
-                    else:
-                        len_occurences[str(len(actual_word))] = 1
-                    actual_word = ""
+        for word in word_list:
+            analyzed_words["total_words"] = analyzed_words.get("total_words", 0) + 1
+            if word.istitle():
+                analyzed_words["titlecase_words"] = analyzed_words.get("titlecase_words", 0) + 1
+            if word.isupper():
+                analyzed_words["uppercase_words"] = analyzed_words.get("uppercase_words", 0) + 1
+            if word.lower():
+                analyzed_words["lowercase_words"] = analyzed_words.get("lowercase_words", 0) + 1
+            if word.isnumeric():
+                analyzed_words["numbers"] = analyzed_words.get("numbers", 0) + 1
+                analyzed_words["numbers_sum"] = analyzed_words.get("numbers_sum", 0) + int(word)
+            len_occurences[f"{len(word)}"] = len_occurences.get(f"{len(word)}", 0) + 1
 
         print("Ve vybraném textu je celkem {0} slov.".format(analyzed_words.get("total_words")))
         print("Slov začínajících velkým písmenem je {0}.".format(analyzed_words.get("titlecase_words")))
